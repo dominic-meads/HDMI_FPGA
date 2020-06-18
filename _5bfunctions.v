46f613667e6bb3ac037ed5a2522b569697c533d6
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2020-06-17 22:43:03 -0700

    update OR DOT function
    I thought OR DOT was an XNOR gate, it is actually just an OR gate. The DOT refers to "emitter dotting" in ECL.

--- _5bfunctions.v
+++ _5bfunctions.v
@@ -10,19 +10,19 @@ module _5bfunction(
 
 // 5b function
 		// intermediate wires
-	assign n_A_nequal_B = ~(A ^ B);  // XNOR: see note at beginning of module
+	assign n_A_nequal_B = ~(A | B) | ~(~A | ~B);  // OR DOT has something to do with ECL, but is just OR
 	assign n_nA_a_nB = ~(~A & ~B);
 	assign n_A_a_B = ~(A & B);
-	assign n_C_nequal_D = ~(C ^ D);
+ 	assign n_C_nequal_D = ~(C | D) | ~(~C | ~D);
 	assign n_nC_a_nD = ~(~C & ~D);
 	assign n_C_a_D = ~(C & D);
 		// end intermediate wires
 		// output wires: "L" means logic function, Lxn = Logic function with "x" ones and "n" zeros (maintain disparity of +2, -2, or 0)
-	assign L40 = ~((n_A_a_B) | (n_C_a_D));  // NOR: see note at beginning of module
-	assign L04 = ~((n_nA_a_nB) | (n_nC_a_nD));
-	assign L13 = ~((n_A_nequal_B) | (n_nC_a_nD)) | ~((n_C_nequal_D) | (n_nA_a_nB));  // two NOR gate equivalents ORed
-	assign L31 = ~((n_A_nequal_B) | (n_C_a_D)) | ~((n_C_nequal_D) | (n_A_a_B));
-	assign L22 = ~((n_A_a_B) | (n_nC_a_nD)) | ~((n_C_a_D) | (n_nA_a_nB)) | ~((n_A_nequal_B) | (n_C_nequal_D)); 
+	assign L40 = ~(n_A_a_B | n_C_a_D);  // NOR: see note at beginning of module
+	assign L04 = ~(n_nA_a_nB | n_nC_a_nD);
+	assign L13 = ~(n_A_nequal_B | n_nC_a_nD) | ~(n_C_nequal_D | n_nA_a_nB);  // two NOR gate equivalents ORed
+	assign L31 = ~(n_A_nequal_B | n_C_a_D) | ~(n_C_nequal_D | n_A_a_B);
+	assign L22 = ~(n_A_a_B | n_nC_a_nD) | ~(n_C_a_D | n_nA_a_nB) | ~(n_A_nequal_B | n_C_nequal_D); 
 		// end output wires
 	// end 5b function
 
