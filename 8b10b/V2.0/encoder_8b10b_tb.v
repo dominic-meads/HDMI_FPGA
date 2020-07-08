d21528e9fe815b0a76c83344e35d3b5ad83ac077
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2020-07-08 00:16:04 -0700

    SEE EXTENDED DESCRIPTION
    7/7/2020 Logic functions work -- see https://www.edaplayground.com/x/rP4
    7/8/2020 5b/6b encoder seems to work (havent tried disparity control yet) -- see https://www.edaplayground.com/x/5uZF

--- 8b10b/V2.0/encoder_8b10b_tb.v
+++ 8b10b/V2.0/encoder_8b10b_tb.v
@@ -1,44 +1,26 @@
 `timescale 1ns / 1ps
 
+`timescale 1ns / 1ps
+
 module tb;
   reg [7:0] data; 
   reg K;
+  reg COMPLS6;
   wire [9:0] out;
   
-  encoder_8b10b uut(data,K,out);
+  encoder_8b10b uut(data,K,COMPLS6,out);
   
   initial 
     begin  // test all possible 4-0, 0-4, 1-3, 3-1, and 2-2 one-zero combos 
       $dumpfile("dump.vcd");
       $dumpvars(0,uut);
       K = 0;
-      data = 8'b11111111;// L40 high:  WORKS
+      COMPLS6 = 0;
+      data = 8'b11100001;  // expect 100010 or 011101 for abcdei
       #1
-      data = 8'b11110000; // L04 high:  WORKS
+      data = 8'b11111100;  // expect 001110 for abcdei
       #1
-      data = 8'b11110001; // L13 high: ALL WORK
-      #1
-      data = 8'b11110010; // L13 high
-      #1
-      data = 8'b11110100; // L13 high
-      #1
-      data = 8'b11111000; // L13 high
-      #1
-      data = 8'b11110111; // L31 high: ALL WORK
-      #1
-      data = 8'b11111110; // L31 high
-      #1
-      data = 8'b11111011; // L31 high
-      #1
-      data = 8'b11111101; // L31 high
-      #1
-      data = 8'b11110011; // L22 high: ALL WORK
-      #1
-      data = 8'b11111100; // L22 high
-      #1
-      data = 8'b11110110; // L22 high
-      #1
-      data = 8'b11111001; // L22 high
+      data = 8'b11110001;  // expect 100011 for abcdei
       #1
       $finish;
     end 
